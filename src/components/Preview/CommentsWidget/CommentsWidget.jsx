6a826ecd4fdb2100eb8ad51dd93bfc65a493fc04
unknown <nastyakoreva1@gmail.com>
2024-02-05 20:53:38 +0500

    Styles for comments

--- src/components/Preview/CommentsWidget/CommentsWidget.jsx
+++ src/components/Preview/CommentsWidget/CommentsWidget.jsx
@@ -3,19 +3,19 @@ import css from "./CommentsWidget.module.css"
 
 const CommentsWidget = (props) => {
     console.log(props);
-    const comments = props.comments.map(c=><div className={css.comment}>
-            <div className={css.user}>
+    const comments = props.comments.map(c => [<div className={css.user}>
                 <div>{c.person_fio}</div>
                 <div className={css.position}>{c.person_position}</div>
-            </div>
-            <div className={css.date}>{c.comment_date}</div>
-            <div className={css.content}>{c.comment}</div>
-        </div>)
+            </div>,
+            <div className={css.date}>{c.comment_date}</div>,
+            <div className={css.content}>{c.comment}</div>])
     return (
 
         <div className={css.comments}> 
-        <div className={css.header}>Комментарий</div> 
+        <div className={css.header}><div className={css.dialogIcon}></div>Комментарий</div> 
+        <div className={css.commentGrid}>
            {comments}
+        </div>
         </div>
     )
 }
