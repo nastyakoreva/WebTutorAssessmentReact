c7c0270992e67cef3274b24410c0261eaeadecd5
unknown <nastyakoreva1@gmail.com>
2024-02-16 15:43:22 +0500

    WorkflowWiget

--- src/components/WorkflowWiget/WorkflowWiget.jsx
+++ src/components/WorkflowWiget/WorkflowWiget.jsx
@@ -7,34 +7,39 @@ const WorkflowWiget = (props) => {
     let pass_flag = true;
     const states = props.workflow_states.map(s => {
         if(s.code !== 'End') {
+            if(!props.boss && s.code === 'Manager' || !props.expert && s.code === 'Approval' || !props.hr && s.code === 'Comitet') return <div/>;
             if(s.code === props.pa.workflow_state) {
                 pass_flag = false;
             }
-            return (<li className={s.code === props.pa.workflow_state ? css.curr : pass_flag && css.pass}>
+            return (<div className={css.wfState}>
+                <div className={`${css.wfDefault} ${s.code === props.pa.workflow_state ? css.curr : pass_flag && css.pass}`}></div>
                 <div>{s.name}</div>
-            </li>)
+            </div>)
         }
         else return null;
     });
 
     return (
         <div className={css.wiget}>
-            <div className={css.workflow}>
-                <p>Процесс оценки</p>
-                <ul>
-                    {states}
-                </ul>
-            </div>
-            <div className={css.experts}>
-                <ul>
-                    <li><UserWiget fullname={props.pa.person_fullname} position={props.pa.person_position_name} role='Оцениваемый' /></li>
-                    {props.boss &&
-                    <li><UserWiget fullname={props.boss.fullname} position={props.boss.position} avatar={props.boss.pict_url} role='Оценивающий руководитель' /></li>}
-                    {props.expert &&
-                    <li><UserWiget fullname={props.expert.fullname} position={props.expert.position} avatar={props.expert.pict_url} role='Согласующий руководитель' /></li>}
-                    {props.hr &&
-                    <li><UserWiget fullname={props.hr.fullname} position={props.hr.position} avatar={props.hr.pict_url} role='HR' /></li>}
-                </ul>
+            <p>Процесс оценки</p>
+            <div className={css.grid}>
+                {[states]}
+
+                <div className={css.expert}>
+                    <UserWiget fullname={props.pa.person_fullname} position={props.pa.person_position_name} role='Оцениваемый' />
+                </div>
+                {props.boss ?
+                <div className={css.expert}>
+                    <UserWiget fullname={props.boss.fullname} position={props.boss.position} avatar={props.boss.pict_url} role='Оценивающий руководитель' />
+                </div> : <div className={css.expertNull}/>}
+                {props.expert ?
+                <div className={css.expert}>
+                    <UserWiget fullname={props.expert.fullname} position={props.expert.position} avatar={props.expert.pict_url} role='Согласующий руководитель' />
+                </div> : <div className={css.expertNull}/>}
+                {props.hr ? 
+                <div className={css.expert}>
+                    <UserWiget fullname={props.hr.fullname} position={props.hr.position} avatar={props.hr.pict_url} role='HR' />
+                </div> : <div className={css.expertNull}/>}
             </div>
         </div>
     )
